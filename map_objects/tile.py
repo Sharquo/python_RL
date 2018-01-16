c38a4570209d653388f42b242f8dbff02a29e11a
Alexander Taxter <Sharquo@gmail.com>
2018-01-16 07:46:18 +0000

    Finished part 4.

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -2,7 +2,7 @@ class Tile:
     """
     A tile on a map.  It may or may not be blocked and it may or may not block sight.
     """
-    def __init__(self, blocked, block_sight = None):
+    def __init__(self, blocked, block_sight=None):
         self.blocked = blocked
 
         # By default, if a tile is blocked, it also blocks sight.
@@ -10,3 +10,6 @@ class Tile:
             block_sight = blocked
 
         self.block_sight = block_sight
+
+        self.explored = False
+        
