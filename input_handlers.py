d663621d9008bf21085c187d0d7869d5630de83e
Alexander Taxter <Sharquo@gmail.com>
2018-01-18 07:48:39 +0000

    Implemented diagonal movement

--- input_handlers.py
+++ input_handlers.py
@@ -1,15 +1,25 @@
 import libtcodpy as libtcod
 
 def handle_keys(key):
+    key_char = chr(key.c)
+
     # Movement keys
-    if key.vk == libtcod.KEY_UP:
+    if key.vk == libtcod.KEY_KP8:
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN:
+    elif key.vk == libtcod.KEY_KP2:
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT:
+    elif key.vk == libtcod.KEY_KP4:
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT:
+    elif key.vk == libtcod.KEY_KP6:
         return {'move': (1, 0)}
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move': (1, -1)}
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move': (1, 1)}
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt + Enter: Toggles the fullscreen.
